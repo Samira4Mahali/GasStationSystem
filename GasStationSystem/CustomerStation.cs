8086272c1a450c0a036ce03adc0c39e67861bede
Samira4Mahali <sazadi687@gmail.com>
2024-10-04 12:01:37 +0330

    Add files via upload

---
+++ GasStationSystem/CustomerStation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasStationSystem
+{
+    internal class CustomerStation
+    {
+        Customer customer;
+        GasStation gasStation;
+        ResponsibleForTheStation ResponsibleForTheStation;
+    }
+}
---
+++ GasStationSystem/GasStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasStationSystem
+{
+    internal class GasStation
+    {
+        public int Id { get; set; }
+        public string StationName { get; set; }
+        public string Location { get; set; }
+        public string GasCapacity { get; set; }
+        public string Facilities { get; set; }
+    }
+}
